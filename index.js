377187ad70b6ecc93a8b0049f81ff6b2688cded3
Lee-jr-81 <129674277+Lee-jr-81@users.noreply.github.com>
2023-10-14 17:10:29 +0100

    Updated the functionality and got the finished code working

--- index.js
+++ index.js
@@ -15,110 +15,11 @@ const idList = [];
 const teamMembers = [];
 
 const appMenu = () => {
-  function buildTeam() {}
-
-  function addIntern() {
-    inquirer
-      .prompt([
-        {
-          type: "input",
-          name: "internName",
-          message: "What is your interns name",
-        },
-        {
-          type: "input",
-          name: "internId",
-          message: "What is your interns id",
-        },
-        {
-          type: "input",
-          name: "internEmail",
-          message: "What is your interns email",
-        },
-        {
-          type: "input",
-          name: "internSchool",
-          message: "What is your interns school",
-        },
-      ])
-      .then((answers) => {
-        const engineer = new Engineer(
-          answers.engineerName,
-          answers.engineerId,
-          answers.engineerEmail,
-          answers.engineerGithub
-        );
-        teamMembers.push(engineer);
-        idList.push(answers.engineerId);
-        console.log(engineer);
-        createTeam();
-      });
-  }
-
-  function addEngineer() {
-    inquirer
-      .prompt([
-        {
-          type: "input",
-          name: "engineerName",
-          message: "What is your engineers name",
-        },
-        {
-          type: "input",
-          name: "engineerId",
-          message: "What is your engineers id",
-        },
-        {
-          type: "input",
-          name: "engineerEmail",
-          message: "What is your engineers email",
-        },
-        {
-          type: "input",
-          name: "engineerGithub",
-          message: "What is your engineers github",
-        },
-      ])
-      .then((answers) => {
-        const engineer = new Engineer(
-          answers.engineerName,
-          answers.engineerId,
-          answers.engineerEmail,
-          answers.engineerGithub
-        );
-        teamMembers.push(engineer);
-        idList.push(answers.engineerId);
-        console.log(engineer);
-        createTeam();
-      });
-  }
-
-  function createTeam() {
-    inquirer
-      .prompt([
-        {
-          type: "list",
-          name: "memberChoice",
-          message: "Which type of team member would you like to add?",
-          choices: [
-            "Engineer",
-            "Intern",
-            "I dont want to add more team members",
-          ],
-        },
-      ])
-      .then((userChoice) => {
-        if (userChoice.memberChoice === "Engineer") {
-          // Add engineer
-          addEngineer();
-        } else if (userChoice.memberChoice === "Intern") {
-          // add Intern
-          addIntern();
-        } else {
-          // build team function
-          buildTeam();
-        }
-      });
+  function buildTeam() {
+    if (!fs.existsSync(OUTPUT_DIR)) {
+      fs.mkdirSync(OUTPUT_DIR);
+    }
+    fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
   }
 
   function createManager() {
@@ -159,10 +60,114 @@ const appMenu = () => {
           answers.managerEmail,
           answers.managerOfficeNumber
         );
-        console.log(manager);
+        //console.log(manager);
         teamMembers.push(manager);
         idList.push(answers.managerId);
         createTeam();
+      });
+  }
+
+  function addEngineer() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "engineerName",
+          message: "What is your engineers name",
+        },
+        {
+          type: "input",
+          name: "engineerId",
+          message: "What is your engineers id",
+        },
+        {
+          type: "input",
+          name: "engineerEmail",
+          message: "What is your engineers email",
+        },
+        {
+          type: "input",
+          name: "engineerGithub",
+          message: "What is your engineers github",
+        },
+      ])
+      .then((answers) => {
+        const engineer = new Engineer(
+          answers.engineerName,
+          answers.engineerId,
+          answers.engineerEmail,
+          answers.engineerGithub
+        );
+        teamMembers.push(engineer);
+        idList.push(answers.engineerId);
+        //console.log(engineer);
+        createTeam();
+      });
+  }
+
+  function addIntern() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "internName",
+          message: "What is your interns name",
+        },
+        {
+          type: "input",
+          name: "internId",
+          message: "What is your interns id",
+        },
+        {
+          type: "input",
+          name: "internEmail",
+          message: "What is your interns email",
+        },
+        {
+          type: "input",
+          name: "internSchool",
+          message: "What is your interns school",
+        },
+      ])
+      .then((answers) => {
+        const intern = new Intern(
+          answers.internName,
+          answers.internId,
+          answers.internEmail,
+          answers.internSchool
+        );
+        teamMembers.push(intern);
+        idList.push(answers.internId);
+        //console.log(intern);
+        createTeam();
+      });
+  }
+
+  function createTeam() {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "memberChoice",
+          message: "Which type of team member would you like to add?",
+          choices: [
+            "Engineer",
+            "Intern",
+            "I dont want to add more team members",
+          ],
+        },
+      ])
+      .then((userChoice) => {
+        if (userChoice.memberChoice === "Engineer") {
+          // Add engineer
+          addEngineer();
+        } else if (userChoice.memberChoice === "Intern") {
+          // add Intern
+          addIntern();
+        } else {
+          // build team function
+          buildTeam();
+        }
       });
   }
 
